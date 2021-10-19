682c418e6ffcbedb046eb4e58815bd1f1b09003f
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-19 08:48:21 -0700

    map & styles

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setenteredGoal] = useState('')
+  const [courseGoals, setcourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setenteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () =>{
+    setcourseGoals(currentGoals => [...currentGoals,enteredGoal])
+  }
+
   return (
-   <View style={{padding:50,flexDirection:'row',width:'80%',height:300,justifyContent:'space-around',alignItems:'stretch'}}>
-     <View style={{backgroundColor:'red',justifyContent:'center',flex:1,alignItems:'center'}}>
-       <Text>1</Text>
+   <View style={styles.screen}>
+     <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal"  style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
+         <Button title="Add" onPress={addGoalHandler} />
      </View>
-     <View style={{backgroundColor:'blue',justifyContent:'center',flex:2,alignItems:'center'}}>
-       <Text>2</Text>
+     <View>
+        {courseGoals.map((goal,index) => <View key={index} style={styles.listItem}><Text>{goal}</Text></View>)}
      </View>
-     <View style={{backgroundColor:'green',justifyContent:'center',flex:3,alignItems:'center'}}>
-       <Text>3</Text>
-     </View>
-   
    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen:{
+    padding:50
+  },
+  inputContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center'
+  },
+  input:{
+    width:200,
+    borderColor:'black' ,
+    borderWidth:1,
+    padding:10
+  },
+  listItem:{
+    padding:10,
+    backgroundColor:'#ccc',
+    borderColor:'black',
+    borderWidth:1,
+    marginVertical:10
+  }
 });

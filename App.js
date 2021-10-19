7a7f951ba4dabdd07afaa4b7a454885b6b01b5ea
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-18 20:28:11 -0700

    flex

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 
 export default function App() {
   return (
-   <View style={{padding:50}}>
-     <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-        <TextInput placeholder="Course Goal"  style={{width:'80%',borderColor:'black' ,borderWidth:1,padding:10}} />
-         <Button title="Add" />
+   <View style={{padding:50,flexDirection:'row',width:'80%',height:300,justifyContent:'space-around',alignItems:'stretch'}}>
+     <View style={{backgroundColor:'red',justifyContent:'center',flex:1,alignItems:'center'}}>
+       <Text>1</Text>
      </View>
-     <View>
-
+     <View style={{backgroundColor:'blue',justifyContent:'center',flex:2,alignItems:'center'}}>
+       <Text>2</Text>
      </View>
+     <View style={{backgroundColor:'green',justifyContent:'center',flex:3,alignItems:'center'}}>
+       <Text>3</Text>
+     </View>
+   
    </View>
   );
 }

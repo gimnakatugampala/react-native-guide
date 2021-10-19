197aad2aca730fde6ed8f2d91f790de973122f64
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-10-19 10:21:59 -0700

    styling buttons

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -6,8 +6,14 @@ export default function GoalInput({addGoalHandler,goalInputHandler,enteredGoal,v
         <Modal animationType="slide" visible={visible}>
         <View style={styles.inputContainer}>
         <TextInput placeholder="Course Goal"  style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
-        <Button title="Cancel" color="red" onPress={cancelGoal} />
-         <Button title="Add" onPress={addGoalHandler} />
+        <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+            <Button title="Cancel" color="red" onPress={cancelGoal} />
+            </View>
+            <View style={styles.button}>
+            <Button title="Add" onPress={addGoalHandler} />
+            </View>
+        </View>
          </View>
         </Modal>
     )
@@ -20,10 +26,18 @@ const styles = StyleSheet.create({
         alignItems:'center'
       },
       input:{
-        width:200,
+        width:'80%',
         borderColor:'black' ,
         borderWidth:1,
         padding:10,
         marginBottom:10
+      },
+      buttonContainer:{
+          flexDirection:'row',
+          justifyContent:'space-between',
+          width:'60%'
+      },
+      button:{
+          width:'40%'
       }
 })
